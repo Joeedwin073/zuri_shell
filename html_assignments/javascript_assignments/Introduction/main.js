641b279b8e4688d5649edd3b47191cb53f67aa0a
Joe Edwin <Joeedwin073@gmail.com>
2022-06-23 22:50:55 +0000

    Javascript added

---
+++ html_assignments/javascript_assignments/Introduction/main.js
@@ -0,0 +1,6 @@
+
+alert("Name: Joe Edwin")
+
+alert("Height: 1.72m")
+
+alert("Country: Nigeria ")
---
+++ javascript_assignments/javascript_intro/intro.js
@@ -0,0 +1,5 @@
+alert("Name: Joe Edwin");
+
+alert("Height: 1.72 meters");
+
+alert("Country: Nigeria");
